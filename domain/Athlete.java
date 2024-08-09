6be2f476e18623b6271fcfec9035d2bac30e61c4
Jose Lucio <jloliveirajunior1903@gmail.com>
2024-08-09 19:48:08 -0300

    Initial commit : implementing Athlete entity

---
+++ domain/Athlete.java
@@ -0,0 +1,86 @@
+package com.example.olympics.com.domain;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "athletes")
+public class Athlete implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private Integer age;
+	private String country;
+	
+	public Athlete() {
+		
+	}
+
+	public Athlete(Long id, String name, Integer age, String country) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.age = age;
+		this.country = country;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Athlete other = (Athlete) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+
+}
