5c012ffde178d497b36dea9cb29b41cc8b2246ab
Jose Lucio <jloliveirajunior1903@gmail.com>
2024-08-09 19:50:31 -0300

    Implementing Athlete Controler class

---
+++ controllers/AthleteController.java
@@ -0,0 +1,63 @@
+package com.example.olympics.com.controllers;
+
+import java.net.URI;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.example.olympics.com.dtos.AthleteDTO;
+import com.example.olympics.com.services.AthleteService;
+
+@RestController
+@RequestMapping("/athletes")
+public class AthleteController {
+
+	@Autowired
+	AthleteService service;
+	
+	@GetMapping
+	public ResponseEntity<List<AthleteDTO>> findAll(){
+		List<AthleteDTO> athletes = service.findAll();
+		return ResponseEntity.ok().body(athletes);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<AthleteDTO> findById(@PathVariable Long id){
+		AthleteDTO athlete = service.findById(id);
+		return ResponseEntity.ok().body(athlete);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Void> create(@RequestBody AthleteDTO athlete){
+		service.create(athlete);
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(athlete.getId())
+                .toUri();
+		return ResponseEntity.created(location).build();
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Void> update(@PathVariable Long id, @RequestBody AthleteDTO athlete){
+		athlete.setId(id);
+		service.update(athlete);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id){
+		service.deleteById(id);
+		return ResponseEntity.noContent().build();
+	}
+
+}
