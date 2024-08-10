0a883867e030657523a496e8f8e2c13a4a146644
Jose Lucio <jloliveirajunior1903@gmail.com>
2024-08-09 22:26:48 -0300

    Implementing Olympics class

---
+++ domain/Olympics.java
@@ -0,0 +1,87 @@
+package com.example.olympics.com.domain;
+
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Olympics implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String hostCity;
+	private Integer year;
+	private String countryWithMostMedals;
+	
+	public Olympics() {
+		
+	}
+
+	public Olympics(Long id, String hostCity, Integer year, String countryWithMostMedals) {
+		super();
+		this.id = id;
+		this.hostCity = hostCity;
+		this.year = year;
+		this.countryWithMostMedals = countryWithMostMedals;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getHostCity() {
+		return hostCity;
+	}
+
+	public void setHostCity(String hostCity) {
+		this.hostCity = hostCity;
+	}
+
+	public Integer getYear() {
+		return year;
+	}
+
+	public void setYear(Integer year) {
+		this.year = year;
+	}
+
+	public String getCountryWithMostMedals() {
+		return countryWithMostMedals;
+	}
+
+	public void setCountryWithMostMedals(String countryWithMostMedals) {
+		this.countryWithMostMedals = countryWithMostMedals;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Olympics other = (Olympics) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+	
+
+}
