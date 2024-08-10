3f09ea43afdbfca8dbeb0e1d00c1e219323a4949
Jose Lucio <jloliveirajunior1903@gmail.com>
2024-08-09 21:50:13 -0300

    Improving swagger documentations

--- controllers/AthleteController.java
+++ controllers/AthleteController.java
@@ -19,6 +19,12 @@ import com.example.olympics.com.dtos.AthleteDTO;
 import com.example.olympics.com.services.AthleteService;
 import com.example.olympics.com.util.MediaType;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+
 @RestController
 @RequestMapping("/athletes")
 public class AthleteController {
@@ -27,18 +33,56 @@ public class AthleteController {
 	AthleteService service;
 	
 	@GetMapping(produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML})
+	@Operation(summary = "Finds all athletes", description = "finds all athletes",
+	tags = {"Athlete"},
+	responses = {
+			@ApiResponse(description = "Success", responseCode = "200",
+					content = {@Content(
+							mediaType = "application/json",
+							array = @ArraySchema(schema = @Schema(implementation = AthleteDTO.class))
+							)}),
+			@ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+			@ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+			@ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+			@ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+			
+	})
 	public ResponseEntity<List<AthleteDTO>> findAll(){
 		List<AthleteDTO> athletes = service.findAll();
 		return ResponseEntity.ok().body(athletes);
 	}
 	
 	@GetMapping(value = "/{id}",produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML})
+	@Operation(summary = "Finds an athlete", description = "finds an athlete",
+	tags = {"Athlete"},
+	responses = {
+			@ApiResponse(description = "Success", responseCode = "200",
+					content = @Content(schema = @Schema(implementation = AthleteDTO.class))
+							),
+			@ApiResponse(description = "No Content", responseCode = "204", content = @Content),
+			@ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+			@ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+			@ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+			@ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+			
+	})
 	public ResponseEntity<AthleteDTO> findById(@PathVariable Long id){
 		AthleteDTO athlete = service.findById(id);
 		return ResponseEntity.ok().body(athlete);
 	}
 	
 	@PostMapping(consumes = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML})
+	@Operation(summary = "Adds a new athlete", description = "adds a new athlete by passing in a JSON, XML or YML representation of the athlete!",
+	tags = {"Athlete"},
+	responses = {
+			@ApiResponse(description = "Success", responseCode = "200",
+					content = @Content),
+			@ApiResponse(description = "Created", responseCode = "201",content = @Content),
+			@ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+			@ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+			@ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+			
+	})
 	public ResponseEntity<Void> create(@RequestBody AthleteDTO athlete){
 		service.create(athlete); 
 		URI location = ServletUriComponentsBuilder.fromCurrentRequest()
@@ -49,6 +93,18 @@ public class AthleteController {
 	}
 	
 	@PutMapping(value = "/{id}",consumes = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML})
+	@Operation(summary = "Updates an athlete", description = "updates an athlete by passing in a JSON, XML or YML representation of the athlete!",
+	tags = {"Athlete"},
+	responses = {
+			@ApiResponse(description = "Updated", responseCode = "200",
+					content = @Content),
+			@ApiResponse(description = "No Content", responseCode = "204",content = @Content),
+			@ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+			@ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+			@ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+			@ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+			
+	})
 	public ResponseEntity<Void> update(@PathVariable Long id, @RequestBody AthleteDTO athlete){
 		athlete.setId(id);
 		service.update(athlete);
@@ -56,6 +112,16 @@ public class AthleteController {
 	}
 	
 	@DeleteMapping(value = "/{id}")
+	@Operation(summary = "Deletes an user", description = "deletes an user by passing their id",
+	tags = {"Athlete"},
+	responses = {
+			@ApiResponse(description = "No Content", responseCode = "204",content = @Content),
+			@ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+			@ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+			@ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+			@ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+			
+	})
 	public ResponseEntity<Void> delete(@PathVariable Long id){
 		service.deleteById(id);
 		return ResponseEntity.noContent().build();
